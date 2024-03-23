2f4266171e3520523a766312d31f9a2b1f5f6fd6
Pashidy <eldafrancis@yahoo.com>
2024-03-23 05:44:04 -0700

    Puppet-lint Tasks

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,9 +1,5 @@
-#!/usr/bin/pup
 # To execute the killmenow command
-
-exec { 'killmenow_process':
-  command     => 'pkill killmenow',
-  refreshonly => true,
-  onlyif      => 'pgrep killmenow',
+exec { 'pkill_killmenow':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
-
