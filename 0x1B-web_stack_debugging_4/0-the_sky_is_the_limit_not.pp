512eace405a95e672eb5738df93b49fba59882d8
Pashidy <eldafrancis@yahoo.com>
2024-05-17 16:46:57 +0000

    Web Debugging4 Tasks

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# A script that fixes the bugs we have found
+
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
+}
